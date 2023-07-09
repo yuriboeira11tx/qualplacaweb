79475ff6b20107b104da459d8c1b67ca8495d353
Yuri Boeira <yuriboeira11tx@gmail.com>
2023-07-09 14:06:48 -0300

    home

--- app/usuarios/usuarioAdm/home.php
+++ app/usuarios/usuarioAdm/home.php
@@ -1,17 +1,17 @@
 <?php
-    require_once 'C:\xampp\htdocs\qualplacaweb\app\conexao_database.php';
+require_once 'C:\xampp\htdocs\qualplacaweb\app\conexao_database.php';
 
-    if (isset($_COOKIE['usuario_logado'])) {
-        $nomeUsuario = explode(",", $_COOKIE['usuario_logado'])[0];
-        $tipoUsuario = explode(",", $_COOKIE['usuario_logado'])[1];
-        if ($tipoUsuario != '0') {
-            header('Location: usuarios/usuarioComum/home.php');
-            exit();
-        }
-    } else {
-        header("Location: usuarios/deslogado.php");
+if (isset($_COOKIE['usuario_logado'])) {
+    $nomeUsuario = explode(",", $_COOKIE['usuario_logado'])[0];
+    $tipoUsuario = explode(",", $_COOKIE['usuario_logado'])[1];
+    if ($tipoUsuario != '0') {
+        header('Location: ../usuarios/usuarioComum/home.php');
         exit();
     }
+} else {
+    header("Location: ../deslogado.php");
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -93,7 +93,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand" href="#">Logo</a>
+        <a class="navbar-brand" href="#"><img src="../../img/logo.png" class="card-img-top" alt="Imagem de login" style="width: 100px;height: 70px;"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -112,63 +112,48 @@
         </div>
     </nav>
     <div class="container">
-        <form>
-            <?php
-            //$sql =  "SELECT P.preco, (select M.nome FROM marca M WHERE M.Id = P.marca_Id), (select F.nome FROM fabricante F WHERE F.Id = P.fabricante_Id), P.vram, P.clock, (select U.nome FROM utilidade U WHERE U.Id = P.utilidade_Id), P.consumo, P.qtdEstrelas FROM placa P WHERE P.preco < '$precoMaximo' AND P.marca_Id = '$marca' AND P.fabricante_Id = '$fabricante' AND P.vram = '$memoria' AND P.clock = '$clock' AND P.utilidade_Id = '$utilidades' AND P.consumo = '$consumo' AND P.qtdEstrelas = '$estrelas';
-            // $sql = "SELECT * FROM placa P WHERE P.qtdEstrelas > 5";
-            $sql = "SELECT * FROM placa P;";
-            $results = mysqli_query($conn, $sql);
+        <?php
+        // Consulta SQL para recuperar todas as placas
+        $sql = "SELECT * FROM placa";
+        $result = $conn->query($sql);
 
-            if (mysqli_num_rows($results) > 0) {
-                while ($result = mysqli_fetch_assoc($results)) :
-            ?>
-            <div class="placa-card">
-                <div class="placa-image">
-                    <img src="<?= $result['path'] ?>">
-                </div>
-                <div class="placa-info">
-                    <h3><?= $result['nome'] ?></h3>
-                    <div class="campo">
-                        <label>Marca:</label>
-                        <span><?= $result['marca_Id'] ?></span>
-                    </div>
-                    <div class="campo">
-                        <label>Fabricante:</label>
-                        <span><?= $result['fabricante_Id'] ?></span>
-                    </div>
-                    <div class="campo">
-                        <label>Memória:</label>
-                        <span><?= $result['vram'] ?></span>
-                    </div>
-                    <div class="campo">
-                        <label>Clock:</label>
-                        <span><?= $result['clock'] ?></span>
-                    </div>
-                    <div class="campo campo-utilidade">
-                        <label>Utilidade(s):</label>
-                        <span><?= $result['utilidade_Id'] ?></span>
-                    </div>
-                    <div class="campo">
-                        <label>Consumo:</label>
-                        <span><?= $result['consumo'] ?></span>
-                    </div>
-                    <div class="campo">
-                        <label>Avaliação:</label>
-                        <span><?= $result['qtdEstrelas'] ?></span>
-                    </div>
-                    <div class="campo">
-                        <label>Preço:</label>
-                        <span><?= $result['preco'] ?></span>
-                        </div>
-                    </div>
-                </div>
-            <?php
-                endwhile;
-            } else {
-                echo '<p class="text-center">Nenhuma placa de vídeo corresponde aos critérios de filtragem.</p>';
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $nome = $row['nome'];
+                $marca_id = $row['marca_Id'];
+                $fabricante_id = $row['fabricante_Id'];
+                $vram = $row['vram'];
+                $clock = $row['clock'];
+                $consumo = $row['consumo'];
+                $imagem = $row['imagem'];
+
+                // Consulta SQL para obter o nome da marca
+                $marca_sql = "SELECT nome FROM marca WHERE id = $marca_id";
+                $marca_result = $conn->query($marca_sql);
+                $marca_nome = $marca_result->fetch_assoc()['nome'];
+
+                // Consulta SQL para obter o nome do fabricante
+                $fabricante_sql = "SELECT nome FROM fabricante WHERE id = $fabricante_id";
+                $fabricante_result = $conn->query($fabricante_sql);
+                $fabricante_nome = $fabricante_result->fetch_assoc()['nome'];
+
+                // Exibe a placa em um card estilizado com Bootstrap
+                echo '<div class="card">';
+                echo '<img src="data:image/jpeg;base64,' . base64_encode($imagem) . '" class="card-img-top" alt="Imagem da Placa">';
+                echo '<div class="card-body">';
+                echo '<h5 class="card-title">' . $nome . '</h5>';
+                echo '<p class="card-text"><strong>Marca:</strong> ' . $marca_nome . '</p>';
+                echo '<p class="card-text"><strong>Fabricante:</strong> ' . $fabricante_nome . '</p>';
+                echo '<p class="card-text"><strong>VRAM:</strong> ' . $vram . '</p>';
+                echo '<p class="card-text"><strong>Clock:</strong> ' . $clock . '</p>';
+                echo '<p class="card-text"><strong>Consumo:</strong> ' . $consumo . '</p>';
+                echo '</div>';
+                echo '</div>';
             }
-            ?>
-        </form>
+        } else {
+            echo 'Nenhuma placa encontrada';
+        }
+        ?>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
@@ -190,55 +175,55 @@
 
 </html>
 
-    <div id="modal-signup" class="modal fade">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Cadastro</h5>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <form method="post">
-                        <div class="form-group">
-                            <label for="signup-name">Nome</label>
-                            <input type="text" class="form-control" id="signup-name" name='nome' placeholder="Digite o nome">
-                        </div>
-                        <div class="form-group">
-                            <label for="signup-email">E-mail</label>
-                            <input type="email" class="form-control" id="signup-email" name='email' placeholder="Digite o e-mail">
-                        </div>
-                        <div class="form-group">
-                            <label for="signup-password">Senha</label>
-                            <input type="password" class="form-control" id="signup-password" name='senha' placeholder="Digite a senha">
-                        </div>
-                        <button type="submit" name="btnCadastro" class="btn btn-primary">Cadastrar</button>
-                    </form>
-                    <?php
-                        if (isset($_POST['btnCadastro']) && ($_POST['nome']) && ($_POST['email']) && ($_POST['senha'])) {
-                            $nome = $_POST['nome'];
-                            $email = $_POST['email'];
-                            $senha = $_POST['senha'];
+<div id="modal-signup" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Cadastro</h5>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <form method="post">
+                    <div class="form-group">
+                        <label for="signup-name">Nome</label>
+                        <input type="text" class="form-control" id="signup-name" name='nome' placeholder="Digite o nome">
+                    </div>
+                    <div class="form-group">
+                        <label for="signup-email">E-mail</label>
+                        <input type="email" class="form-control" id="signup-email" name='email' placeholder="Digite o e-mail">
+                    </div>
+                    <div class="form-group">
+                        <label for="signup-password">Senha</label>
+                        <input type="password" class="form-control" id="signup-password" name='senha' placeholder="Digite a senha">
+                    </div>
+                    <button type="submit" name="btnCadastro" class="btn btn-primary">Cadastrar</button>
+                </form>
+                <?php
+                if (isset($_POST['btnCadastro']) && ($_POST['nome']) && ($_POST['email']) && ($_POST['senha'])) {
+                    $nome = $_POST['nome'];
+                    $email = $_POST['email'];
+                    $senha = $_POST['senha'];
 
-                            // Executa a query de inserção
-                            $query = "INSERT INTO usuario (nome, email, senha, tipo) VALUES ('$nome', '$email', '$senha', '0')";
-                            $resultado = mysqli_query($conn, $query);
+                    // Executa a query de inserção
+                    $query = "INSERT INTO usuario (nome, email, senha, tipo) VALUES ('$nome', '$email', '$senha', '0')";
+                    $resultado = mysqli_query($conn, $query);
 
-                            // Verifica se o cadastro foi realizado com sucesso
-                            if ($resultado) {
-                                echo '<script>alert("Usuário cadastrado com sucesso!");</script>';
-                            } else {
-                                echo '<script>alert("Erro ao cadastrar '. mysqli_error($conn) . ' ")' . ';</script>';
-                            }
-                        }
-                    ?>
-                </div>
+                    // Verifica se o cadastro foi realizado com sucesso
+                    if ($resultado) {
+                        echo '<script>alert("Usuário cadastrado com sucesso!");</script>';
+                    } else {
+                        echo '<script>alert("Erro ao cadastrar ' . mysqli_error($conn) . ' ")' . ';</script>';
+                    }
+                }
+                ?>
             </div>
         </div>
     </div>
+</div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
